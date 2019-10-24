9445f0408a293902263315e2cefea6c96b0ea351
Christian López <clopez@bechemsud.local>
2019-10-24 16:18:23 -0300

    API Courses controller

---
+++ Xamarin/Xamarin.Web/Controllers/API/CoursesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Xamarin.Web.Data;
+
+namespace Xamarin.Web.Controllers.API
+{
+    [Route("api/[Controller]")]
+    public class CoursesController : Controller
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public CoursesController(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetCourses()
+        {
+            return Ok(_courseRepository.GetAll());
+        }
+    }
+}
